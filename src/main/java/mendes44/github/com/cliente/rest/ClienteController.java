452de808ddb73e47919cd110d9da9723b2c612c2
Mendes44 <marcosmendesm10@gmail.com>
2024-01-19 06:21:12 -0300

    Update: Criando metodo atualizar

--- src/main/java/mendes44/github/com/cliente/rest/ClienteController.java
+++ src/main/java/mendes44/github/com/cliente/rest/ClienteController.java
@@ -49,4 +49,16 @@ public class ClienteController {
                 })
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
+
+    @PutMapping({"id"})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void atualizar(@PathVariable Long id, @RequestBody Cliente clienteAtualizado){
+        repository
+                .findById(id)
+                .map( cliente -> {
+                    clienteAtualizado.setId();
+                    return repository.save( clienteAtualizado );
+                })
+                .orElseThrow(()  -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
 }
