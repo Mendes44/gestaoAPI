3410b4404ed23836b2c2a45c0550f3469b65f7ae
Mendes44 <marcosmendesm10@gmail.com>
2024-01-22 06:29:01 -0300

    POM UPDATE

--- src/main/java/mendes44/github/com/cliente/rest/ClienteController.java
+++ src/main/java/mendes44/github/com/cliente/rest/ClienteController.java
@@ -1,5 +1,6 @@
 package mendes44.github.com.cliente.rest;
 
+import jakarta.validation.Valid;
 import mendes44.github.com.cliente.entity.Cliente;
 import mendes44.github.com.cliente.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,26 +8,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collections;
+
 
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    //Utilizo o construtor padrão por bons costumes pois poderia fazer assim:
-    //@Autowired
-    //private ClienteRepositoru repository;
     private final ClienteRepository repository;
 
     @Autowired
     public ClienteController( ClienteRepository repository ){
+
         this.repository = repository;
     }
 
     //Metodo que indica que busca atrasves do REST e Salva no banco de dados com status 201
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public void salvar ( @RequestBody Cliente cliente){
+    public void salvar ( @Valid @RequestBody Cliente cliente){
         repository.save(cliente);
     }
 
